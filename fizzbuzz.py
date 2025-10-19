d6daff16d1e2c1a99a3260e187a3ab90f1da1970
Lisan Sritharan <lisan2003.s@gmail.com>
2025-10-20 00:03:32 +0200

    3ème code qui succes

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,6 +1,6 @@
-def affiche(n=100):
+def affiche(n1=1, n2=100):
     resultat = ""
-    for i in range(1, n + 1): 
+    for i in range(n1,n2 + 1): 
         if i % 15 == 0:
             resultat += "FrisBee"
         elif i % 3 == 0:
